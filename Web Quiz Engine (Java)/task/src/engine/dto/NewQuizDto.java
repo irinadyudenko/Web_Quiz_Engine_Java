b5765926714d7086e2f56b4690b05435f4ca90a9
Irina Diudenko <irina.dyudenko@gmail.com>
2023-11-07 16:34:03 +0100

    Added @Validated annotation and @NotNull constraint to options field

--- Web Quiz Engine (Java)/task/src/engine/dto/NewQuizDto.java
+++ Web Quiz Engine (Java)/task/src/engine/dto/NewQuizDto.java
@@ -3,21 +3,28 @@ package engine.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Validated
 public class NewQuizDto {
 
     @NotBlank
     private String title;
+
     @NotBlank
     private String text;
+
     @Size(min = 2)
+    @NotNull
     private List<String> options;
+
     private List<Integer> answer;
 }
