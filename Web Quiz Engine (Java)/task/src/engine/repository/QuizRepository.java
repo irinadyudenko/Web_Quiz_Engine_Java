6e5ebd0e73a8c738fdb5879f2244e722c6b94025
Irina Diudenko <irina.dyudenko@gmail.com>
2023-11-07 14:26:06 +0100

    Created QuizRepository class

---
+++ Web Quiz Engine (Java)/task/src/engine/repository/QuizRepository.java
@@ -0,0 +1,8 @@
+package engine.repository;
+
+import engine.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+}
